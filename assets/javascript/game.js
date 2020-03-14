9164679cd835b0fe9c2a01f1b76fb5fd460152cd
Eric Kinsley <dt1993@gmail.com>
2020-03-14 11:06:09 -0500

    update js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -31,3 +31,59 @@ let resetGame = () => {
 	guessesSoFar = [];
 	pyschic();
 };
+
+
+
+// Grab users keystroke to intput there guess at the letter and start the game
+document.onkeyup = (event) => {
+
+	userGuess = event.key.toLowerCase();
+
+	pyschic();
+
+	console.log(userGuess);
+
+
+	if (userGuess == pyschicChoice) {
+		correct++;
+		alertWin();
+	} 
+	if (userGuess != pyschicChoice) {
+		guessesLeft--;
+		guessesSoFar.push(userGuess);
+	
+
+
+
+	// Output to place on the webpage
+	let html = 
+		"<p>Guesses Left </p>" +
+		"<p>"+ guessesLeft + "</p>" +
+		"<p>Letters Guessed</p>" + 
+		"<p>"+ guessesSoFar + "</p>" +
+		"<br>" +
+		"<p>Correct </p>" + 
+		"<p>"+ correct + "</p>" +
+		"<p>Incorrect </p>" + 
+		"<p>"+ incorrect + "</p>";
+
+	// Set the inner HTML contents of the game_output id to our html string
+    document.querySelector("#game_output").innerHTML = html;
+
+    if ( guessesLeft === 0 ) {
+		incorrect++;
+		alertLoss();
+		}
+	}
+
+};
+
+let alertWin = () => {
+	alert("Congratulations I was thinking of " + pyschicChoice + ".");
+	resetGame();
+};
+
+let alertLoss = () => {
+	alert("I am sorry you are incorrect, I was thinking of " + pyschicChoice + ". " + "You Guessed " + userGuess + ".");
+	resetGame();
+};
